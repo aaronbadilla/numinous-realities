3e528660c8bbc868866fc8bc0dd9e75165d2fd24
Aaron Badilla <aaronbadilla@Aarons-MacBook-Pro.local>
2021-08-12 16:12:47 -0400

    migrated image files to google cloud

--- src/pages/gallery-1/gallery-1.data.jsx
+++ src/pages/gallery-1/gallery-1.data.jsx
@@ -3,7 +3,7 @@ const PagesInfo = [
         title: '"What happened?"',
         link: 'https://ab2.gallery/listing/291995654',
         className: 'pages',
-        image: 'https://gateway.pinata.cloud/ipfs/QmXbFNhRBVXWmfHJ1mfS3xpnan2RgwKbw8kT6C2vDMkkd4',
+        image: 'https://storage.googleapis.com/numinous-realities/Numinous%20Realities%20-%20Page%201%20-%20What%20happened%3F.jpg',
         alt:'what happened?',
         description: '"What happened? Depicts the artists struggle to focus on the task at hand. A feeling shared by many artists especially at the beginning of a work session',
         price: '1'
@@ -12,7 +12,7 @@ const PagesInfo = [
         title: '"forgot yourself"',
         link: 'https://ab2.gallery/listing/284861954',
         className: 'pages',
-        image: 'https://gateway.pinata.cloud/ipfs/Qme5gaM4FdUvBLm7VyYGqfwzh8g7eMjwdEZENN823WK1eH',
+        image: 'https://storage.googleapis.com/numinous-realities/Numinous%20Realities%20-%20Page%202.jpg',
         alt:'forgot yourself',
         description: 'In a reaction to an unruly brain, the artist attempts to find focus by remembering who they are.',
         price: '1'
@@ -21,7 +21,7 @@ const PagesInfo = [
         title: '"yawn"',
         link: 'https://ab2.gallery/listing/284862539',
         className: 'pages',
-        image: 'https://gateway.pinata.cloud/ipfs/QmPd22cRwJk2T1sdnhfhUpj2n86eoKhXgtjMoPhbb9GS34',
+        image: 'https://storage.googleapis.com/numinous-realities/Numinous%20Realities%20-%20Page%203.jpg',
         alt: 'yawn',
         description: 'The subconscious brain, always trying to conserve energy, reminds the artist that their time could be better spent sleeping. ',
         price: '1'
@@ -30,7 +30,7 @@ const PagesInfo = [
         title: '"HELLO"',
         link: 'https://ab2.gallery/listing/284863195',
         className: 'pages',
-        image: 'https://gateway.pinata.cloud/ipfs/QmbpS6nvPxzoASWa3FLcnLWcVLa4JNAj6WS8x7QF7wtUsd',
+        image: 'https://storage.googleapis.com/numinous-realities/Numinous%20Realities%20-%20Page%204.jpg',
         alt:'hello',
         description: 'A banana telephone, perhaps summoned by the artists desire to wake themselves up, is summoned.',
         price: '1'
@@ -39,7 +39,7 @@ const PagesInfo = [
         title: '"ahm comfy"',
         link: 'https://ab2.gallery/listing/284864199',
         className: 'pages',
-        image: 'https://gateway.pinata.cloud/ipfs/QmPbprbt7AmnHb8ytwwgC2ofUJMURGHi7pn8ET7Ni7ykJf',
+        image: 'https://storage.googleapis.com/numinous-realities/Numinous%20Realities%20-%20Page%205.jpg',
         alt: 'ahm comfy',
         description: 'Uh oh, perhaps the subconscious brain will get what it wants after all.',
         price: '1'
@@ -48,7 +48,7 @@ const PagesInfo = [
         title: '"and then i woke up a beetle"',
         link: 'https://ab2.gallery/listing/284864790',
         className: 'pages',
-        image: 'https://gateway.pinata.cloud/ipfs/QmYPcyer9WCi8YaFtziMnNeQrXGLekNDvh4JKe79WdDju9',
+        image: 'https://storage.googleapis.com/numinous-realities/Numinous%20Realities%20-%20Page%206.jpg',
         alt: 'and then i woke up a beetle',
         description: 'A breakthrough? Will the subconscious brain provide some creativity after all? ',
         price: '1'
@@ -57,7 +57,7 @@ const PagesInfo = [
         title: '"What you are looking for, BE"',
         link: 'https://ab2.gallery/listing/284502068',
         className: 'pages',
-        image: 'https://gateway.pinata.cloud/ipfs/QmZnbxPECGnJGyqt9vuoWJJ21x53NZWsPLZEpsCCFnzwdn',
+        image: 'https://storage.googleapis.com/numinous-realities/Numinous%20Realities%20-%20Page%207.jpg',
         alt: 'be',
         description: `Ah, the artist has created something now they can relax and just be.`,
         price: '1'
@@ -66,7 +66,7 @@ const PagesInfo = [
         title: '"YOU LEECH"',
         link: 'https://ab2.gallery/listing/286031578',
         className: 'pages',
-        image: 'https://gateway.pinata.cloud/ipfs/QmZFQ4rZpPnPxHMqLWV8dpRQDKCPc2hBggFDYxEzcqw7r5',
+        image: 'https://storage.googleapis.com/numinous-realities/Numinous%20Realities%20-%20Page%208.jpg',
         alt:'you leech',
         description: 'Uh oh, the artists brain does not like the sound of that and fights back with the age old trick of self-doubt',
         price: '1'
@@ -75,7 +75,7 @@ const PagesInfo = [
         title: '"HOW COULD YOU LET US DOWN"',
         link: 'https://ab2.gallery/listing/287699081',
         className: 'pages',
-        image: 'https://gateway.pinata.cloud/ipfs/QmdzqKWwRjpRSj4F3SUDDPzRG3mThkttpWktnZM97MpSiE',
+        image: 'https://storage.googleapis.com/numinous-realities/Numinous%20Realities%20-%20Page%209.jpg',
         alt:'how could you let us down',
         description: 'This is not looking good, the artist is now spiraling into self-loathing.',
         price: '1'
@@ -84,7 +84,7 @@ const PagesInfo = [
         title: '"my thoughts are interesting again"',
         link: 'https://ab2.gallery/listing/289608672',
         className: 'pages',
-        image: 'https://gateway.pinata.cloud/ipfs/QmZN82StD5LPcxTzi9Py8LcXmJypwVSxZDPGbxut8KkiHU',
+        image: 'https://storage.googleapis.com/numinous-realities/Numinous%20Realities%20-%20Page%2010.jpg',
         alt:'my thoughts are interesting again',
         description: 'What a pleasant surprise, perhaps the visual interpretation of letting someone down sparked some creative excitement.',
         price: '1'
@@ -93,7 +93,7 @@ const PagesInfo = [
         title: `"you're my anam cara"`,
         link: '',
         className: 'pages',
-        image: 'https://gateway.pinata.cloud/ipfs/QmfDSRhSLwnv69utL2UFGBFRbbHb1mWqTykgrhsiGUKDad',
+        image: 'https://storage.googleapis.com/numinous-realities/Numinous%20Realities%20-%20Page%2011.jpg',
         alt:"you're my anam cara",
         description: 'An anam cara or "soul friend" hints at a deep personal relationship, the thought of which seems to have stabilized the artist for now. ',
         price: '1'
@@ -102,7 +102,7 @@ const PagesInfo = [
         title: '"CELEBRATE"',
         link: '',
         className: 'pages',
-        image: 'https://gateway.pinata.cloud/ipfs/QmZFjGLeov11gVxo21cgFprA7ybhgTsnrRktus3kBg1RSD',
+        image: 'https://storage.googleapis.com/numinous-realities/Numinous%20Realities%20-%20Page%2012.jpg',
         alt:'celebrate',
         description: 'Using the spring board of personal relationships the artist opens their mind to the community as a whole.',
         price: '1'
